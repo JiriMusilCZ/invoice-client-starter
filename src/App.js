acfd926cffb44ec337c46d5c1384c24015b4ae20
JiriMusilCZ <jiri.musil2012@gmail.com>
2024-07-23 13:14:21 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+/*  _____ _______         _                      _
+ * |_   _|__   __|       | |                    | |
+ *   | |    | |_ __   ___| |___      _____  _ __| | __  ___ ____
+ *   | |    | | '_ \ / _ \ __\ \ /\ / / _ \| '__| |/ / / __|_  /
+ *  _| |_   | | | | |  __/ |_ \ V  V / (_) | |  |   < | (__ / /
+ * |_____|  |_|_| |_|\___|\__| \_/\_/ \___/|_|  |_|\_(_)___/___|
+ *                                _
+ *              ___ ___ ___ _____|_|_ _ _____
+ *             | . |  _| -_|     | | | |     |  LICENCE
+ *             |  _|_| |___|_|_|_|_|___|_|_|_|
+ *             |_|
+ *
+ *   PROGRAMOVÁNÍ  <>  DESIGN  <>  PRÁCE/PODNIKÁNÍ  <>  HW A SW
+ *
+ * Tento zdrojový kód je součástí výukových seriálů na
+ * IT sociální síti WWW.ITNETWORK.CZ
+ *
+ * Kód spadá pod licenci prémiového obsahu a vznikl díky podpoře
+ * našich členů. Je určen pouze pro osobní užití a nesmí být šířen.
+ * Více informací na http://www.itnetwork.cz/licence
+ */
+
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+import PersonIndex from "./persons/PersonIndex";
+import PersonDetail from "./persons/PersonDetail";
+import PersonForm from "./persons/PersonForm";
+
+export function App() {
+  return (
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/persons"} className="nav-link">
+                Osoby
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route index element={<Navigate to={"/persons"} />} />
+          <Route path="/persons">
+            <Route index element={<PersonIndex />} />
+            <Route path="show/:id" element={<PersonDetail />} />
+            <Route path="create" element={<PersonForm />} />
+            <Route path="edit/:id" element={<PersonForm />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/persons/PersonDetail.js
@@ -0,0 +1,80 @@
+/*  _____ _______         _                      _
+ * |_   _|__   __|       | |                    | |
+ *   | |    | |_ __   ___| |___      _____  _ __| | __  ___ ____
+ *   | |    | | '_ \ / _ \ __\ \ /\ / / _ \| '__| |/ / / __|_  /
+ *  _| |_   | | | | |  __/ |_ \ V  V / (_) | |  |   < | (__ / /
+ * |_____|  |_|_| |_|\___|\__| \_/\_/ \___/|_|  |_|\_(_)___/___|
+ *                                _
+ *              ___ ___ ___ _____|_|_ _ _____
+ *             | . |  _| -_|     | | | |     |  LICENCE
+ *             |  _|_| |___|_|_|_|_|___|_|_|_|
+ *             |_|
+ *
+ *   PROGRAMOVÁNÍ  <>  DESIGN  <>  PRÁCE/PODNIKÁNÍ  <>  HW A SW
+ *
+ * Tento zdrojový kód je součástí výukových seriálů na
+ * IT sociální síti WWW.ITNETWORK.CZ
+ *
+ * Kód spadá pod licenci prémiového obsahu a vznikl díky podpoře
+ * našich členů. Je určen pouze pro osobní užití a nesmí být šířen.
+ * Více informací na http://www.itnetwork.cz/licence
+ */
+
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+
+import {apiGet} from "../utils/api";
+import Country from "./Country";
+
+const PersonDetail = () => {
+    const {id} = useParams();
+    const [person, setPerson] = useState({});
+
+    useEffect(() => {
+        // TODO: Add HTTP req.
+    }, [id]);
+    const country = Country.CZECHIA === person.country ? "Česká republika" : "Slovensko";
+
+    return (
+        <>
+            <div>
+                <h1>Detail osoby</h1>
+                <hr/>
+                <h3>{person.name} ({person.identificationNumber})</h3>
+                <p>
+                    <strong>DIČ:</strong>
+                    <br/>
+                    {person.taxNumber}
+                </p>
+                <p>
+                    <strong>Bankovní účet:</strong>
+                    <br/>
+                    {person.accountNumber}/{person.bankCode} ({person.iban})
+                </p>
+                <p>
+                    <strong>Tel.:</strong>
+                    <br/>
+                    {person.telephone}
+                </p>
+                <p>
+                    <strong>Mail:</strong>
+                    <br/>
+                    {person.mail}
+                </p>
+                <p>
+                    <strong>Sídlo:</strong>
+                    <br/>
+                    {person.street}, {person.city},
+                    {person.zip}, {country}
+                </p>
+                <p>
+                    <strong>Poznámka:</strong>
+                    <br/>
+                    {person.note}
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default PersonDetail;
